412848676861f0bdc9dfec51bdac563f638635f0
choejihwan <ghldtjd901@naver.com>
2023-09-07 22:45:10 +0900

    feat/init_project: 처음 프로젝트 시작.

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Interval"
+include(":app")
+include(":interval")
+include(":timer")
+include(":notification")
