f3fb06333b2122b2811de6edccfcd80909cb4bfd
choejihwan <ghldtjd901@naver.com>
2023-09-10 13:41:35 +0900

    feat/navigation: 싱글 액티비티를 위해 네비게이션 설정 추가 및 테스트 화면 추가.

--- app/src/main/java/com/bof/android/interval/MainActivity.kt
+++ app/src/main/java/com/bof/android/interval/MainActivity.kt
@@ -3,44 +3,33 @@ package com.bof.android.interval
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.bof.android.interval.ui.theme.IntervalTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.bof.android.interval.presentation.IntervalMainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            IntervalTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            // 앱의 네비게이션 설정. 함수 내부에서 전체 router의 NavController 정의.
+            AppNavigator()
         }
     }
 }
 
+/**
+ * 앱 내부 네비게이션 설정.
+ * 사용법:
+ * 1. 선언: composable("태그 이름") { 화면 컴포즈 함수 }
+ * 2. 불러오기: navController.navigate("태그 이름")
+ */
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IntervalTheme {
-        Greeting("Android")
+fun AppNavigator() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") { IntervalMainScreen(navController = navController) }
+        composable("detail") { /* todo anotherScreen */ }
     }
 }
---
+++ app/src/main/java/com/bof/android/interval/presentation/IntervalMain.kt
@@ -0,0 +1,115 @@
+package com.bof.android.interval.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+/**
+ * 인터벌 메인 화면의 전체 화면.
+ */
+@Composable
+fun IntervalMainScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top
+    ) {
+        // 최상단 앱바
+        AppBar(title = "", onBackClicked = { navController.popBackStack() })
+        // 투명 디바이더 역할
+        Spacer(modifier = Modifier.height(20.dp))
+        // todo 타이머 위치.
+        Timer()
+        // todo 타이머랑 버튼이 같이 만들어질 것 같으나, 테스트 용으로.
+        Buttons()
+    }
+}
+
+/**
+ * 최상단 앱바.
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(
+    title: String,
+    onBackClicked: () -> Unit
+) {
+    TopAppBar(
+        title = { Text(text = title) },
+        navigationIcon = {
+            IconButton(onClick = { onBackClicked() }) {
+                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "back")
+            }
+        }
+    )
+}
+
+/**
+ * 실질적인 Timer가 들어갈 예정. 로직은 대신 Timer 모듈에서 가져온다.
+ */
+@Composable
+fun Timer() {
+    Surface {
+        Text(text = "이 곳에 타이머가 들어갑니다.")
+    }
+}
+
+/**
+ * 테스트용 버튼 묶음.(나중에 필요 없을 예정)
+ */
+@Composable
+fun Buttons() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        IconButton(onClick = {}) {
+            Icon(imageVector = Icons.Default.PlayArrow, contentDescription = "play")
+        }
+        IconButton(onClick = {}) {
+            Icon(imageVector = Icons.Default.PlayArrow, contentDescription = "play")
+        }
+        IconButton(onClick = {}) {
+            Icon(imageVector = Icons.Default.PlayArrow, contentDescription = "play")
+        }
+        IconButton(onClick = {}) {
+            Icon(imageVector = Icons.Default.PlayArrow, contentDescription = "play")
+        }
+    }
+}
+
+// ---------------------- preview line -------------------------
+//
+
+@Composable
+@Preview
+fun IntervalMainScreenPreview() {
+    IntervalMainScreen(rememberNavController())
+}
+
+@Composable
+@Preview
+fun TimerPreview() {
+    Timer()
+}
