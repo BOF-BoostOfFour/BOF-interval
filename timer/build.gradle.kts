ac01d2ad46b0ad2934dc8ce87709737f46e19549
choejihwan <ghldtjd901@naver.com>
2023-09-23 11:00:21 +0900

    chore/hilt: hilt 종속성 추가.

--- timer/build.gradle.kts
+++ timer/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    // ksp plugin.
+    id("com.google.devtools.ksp")
+    // hilt plugin.
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -37,6 +41,11 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
+
+    // hilt dependency.
+    implementation("com.google.dagger:hilt-android:2.44")
+    ksp("com.google.dagger:hilt-android-compiler:2.44")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
